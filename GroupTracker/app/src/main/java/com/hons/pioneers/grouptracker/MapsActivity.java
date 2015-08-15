f3d8d288321fb899cb1f17e3f0d337cd410929e3
prossouw79 <prossouw79@gmail.com>
2015-08-15 15:30:50 +0200

    0.1

--- GroupTracker/app/src/main/java/com/hons/pioneers/grouptracker/MapsActivity.java
+++ GroupTracker/app/src/main/java/com/hons/pioneers/grouptracker/MapsActivity.java
@@ -1,12 +1,20 @@
 package com.hons.pioneers.grouptracker;
 
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -17,12 +25,15 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        Handler handler = new Handler();
+        handler.postDelayed(mRunnable,100);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
+        setUpMap();
     }
 
     /**
@@ -52,7 +63,14 @@ public class MapsActivity extends FragmentActivity {
             }
         }
     }
+    private final Runnable mRunnable = new Runnable()
+    {
+        public void run()
+        {
+            setUpMap();
+        }
 
+    };
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
@@ -60,6 +78,101 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(-27.1033, 26.7150)).title("Potchefstroom!"));
+            mMap.clear();
+            List<MapObject> mapObjects = getObjects("myID");
+            for(MapObject ob : mapObjects) {
+                if (ob instanceof Person) {
+                    Person obj = (Person) ob;
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(obj.getLat(), obj.getLong())).title(obj.getTitle()).snippet(obj.getSnip()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                }
+                else {
+                    MapMarker obj = (MapMarker) ob;
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(obj.getLat(), obj.getLong())).title(obj.getTitle()).snippet(obj.getSnip()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+                }
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+    }
+
+
+
+
+    private List<MapObject> getObjects(String id) {
+        Random r = new Random();
+
+        List<MapObject> list = new ArrayList<MapObject>();
+        list.clear();
+
+        for (int i = 0; i < 5; i++)
+            list.add(new Person(r.nextFloat() * (100 - 20) + 20, r.nextFloat() * (100 - 20) + 20, Integer.toString(r.nextInt())));
+        for (int i = 0; i < 5; i++)
+            list.add(new MapMarker(r.nextFloat() * (100 - 20) + 20, r.nextFloat() * (100 - 20) + 20, Integer.toString(r.nextInt())));
+
+        return list;
+    }
+
+    private class MapObject
+    {
+        float lon;
+        float lat;
+        String title;
+        String snip = "snippet here";
+    }
+
+    private class Person extends MapObject
+    {
+        public Person(float lonv, float latv, String titlev)
+        {
+            super.lon = lonv;
+            super.lat = latv;
+            super.title = titlev;
+        }
+
+        public float getLat() {
+            return super.lat;
+        }
+
+        public float getLong(){
+            return super.lon;
+        }
+
+        public String getTitle(){
+            return super.title;
+        }
+
+        public String getSnip(){
+            return super.snip;
+        }
+    }
+
+    private class MapMarker extends MapObject
+    {
+        public MapMarker(float lonv, float latv, String titlev)
+        {
+            super.lon = lonv;
+            super.lat = latv;
+            super.title = titlev;
+        }
+
+        public float getLat() {
+            return super.lat;
+        }
+
+        public float getLong(){
+            return super.lon;
+        }
+
+        public String getTitle(){
+            return super.title;
+        }
+
+        public String getSnip(){
+            return super.snip;
+        }
     }
 }
+
+
